265a58fbc5f0a7b624c865c782ad43903ad84c52
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2018-07-26 11:00:22 +0900

    Add parameter NODE_TO_ATTACH (#147)
    This commit enables a parameter when you execute attach.sh.
    Previously, attach.sh attaches only to the first node by default.
    Using `./attach.sh 1`, you can attach to the second node.
    
    Signed-off-by: Junghyun Colin Kim <colin.kim@groundx.xyz>

--- scripts/attach.sh
+++ scripts/attach.sh
@@ -2,5 +2,15 @@
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SOURCE_DIR/common.sh
+NODE_TO_ATTACH=0
 
-$GXP_BIN_PATH/gxp attach http://localhost:$RPC_PORT_START
+if [ -n "$1" ]; then
+  NODE_TO_ATTACH=$1
+fi
+
+RPC_PORT=$(( ($RPC_PORT_START + $NODE_TO_ATTACH * 10000) % 65536))
+
+ATTACH="http://localhost:$RPC_PORT"
+echo "!!!Attaching to $ATTACH"
+
+$GXP_BIN_PATH/gxp attach $ATTACH
