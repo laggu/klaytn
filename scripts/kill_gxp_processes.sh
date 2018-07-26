d3ce8f168bc15413f4f071d599e614d777f2b6ba
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2018-07-26 18:34:14 +0900

    Remove unnecessary scripts. (#163)
    Signed-off-by: Junghyun Colin Kim <colin.kim@groundx.xyz>

--- scripts/kill_gxp_processes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ps -ef | grep "gxp --datadir" | awk '{print $2}' | xargs -I{} kill -9 {}
--- scripts/run_gxp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./istanbul_setup.sh
-./gen_genesis_block.sh
-./launch_nodes.sh
