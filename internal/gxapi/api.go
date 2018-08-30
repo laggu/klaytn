a4613da081ca6f8b5718ddd4dcaf6f94b50e0662
Hyung-Kyu Hqueue Choi <hyungkyu.choi@gmail.com>
2018-08-30 21:44:52 +0900

    Disable enforcing gasPrice to 0 in SendTransaction API (#409)
    - Disable enforcing gasPrice to 0 in SendTransaction API
    
    Signed-off-by: Hyung-Kyu Hqueue Choi <hqueue.choi@groundx.xyz>

--- internal/gxapi/api.go
+++ internal/gxapi/api.go
@@ -1231,9 +1231,6 @@ func submitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (c
 // SendTransaction creates a transaction for the given argument, sign it and submit it to the
 // transaction pool.
 func (s *PublicTransactionPoolAPI) SendTransaction(ctx context.Context, args SendTxArgs) (common.Hash, error) {
-	// Set GasPrice to 0
-	args.GasPrice =(*hexutil.Big)(big.NewInt(0))
-
 	// Look up the wallet containing the requested signer
 	account := accounts.Account{Address: args.From}
 
