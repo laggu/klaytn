ee763d32d7f95be787d55d0ed77aac6a8e0231b3
Jesse Jonghui Lee <jesse.lee@groundx.xyz>
2018-09-05 21:34:38 +0900

    Release v0.3.0-2 (#441)
    Signed-off-by: jesse.lee <jesse.lee@groundx.xyz>

--- params/version.go
+++ params/version.go
@@ -19,10 +19,10 @@ package params
 import "fmt"
 
 const (
-	ReleaseNum   = 1
+	ReleaseNum   = 2
 	VersionMajor = 0 // Major version component of the current release
-	VersionMinor = 2 // Minor version component of the current release
-	VersionPatch = 3 // Patch version component of the current release
+	VersionMinor = 3 // Minor version component of the current release
+	VersionPatch = 0 // Patch version component of the current release
 )
 
 // Version holds the textual version string.
