45753a464e447f2ba3e14ffdb6a29017a5da6d73
juliaHong0524 <43627820+juliaHong0524@users.noreply.github.com>
2018-10-01 13:26:35 +0900

    Release v0.4.1-6 (#535)

--- params/version.go
+++ params/version.go
@@ -19,10 +19,10 @@ package params
 import "fmt"
 
 const (
-	ReleaseNum   = 5
+	ReleaseNum   = 6
 	VersionMajor = 0 // Major version component of the current release
 	VersionMinor = 4 // Minor version component of the current release
-	VersionPatch = 0 // Patch version component of the current release
+	VersionPatch = 1 // Patch version component of the current release
 )
 
 // Version holds the textual version string.
