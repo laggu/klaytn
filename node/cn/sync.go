424f0f936fd1137409f44601846f2d47365418ce
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2018-10-08 17:05:59 +0900

    enabled previously commented out defer pm.downloader.Terminate() (#539)

--- node/cn/sync.go
+++ node/cn/sync.go
@@ -118,7 +118,7 @@ func (pm *ProtocolManager) syncer() {
 	// Start and ensure cleanup of sync mechanisms
 	pm.fetcher.Start()
 	defer pm.fetcher.Stop()
-	//defer pm.downloader.Terminate()
+	defer pm.downloader.Terminate()
 
 	// Wait for different events to fire synchronisation operations
 	forceSync := time.NewTicker(forceSyncCycle)
