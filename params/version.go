7cc42466ad68ed54da74fc8e11b563cb840e3944
jesse.lee <jesse.lee@groundx.xyz>
2018-10-24 13:52:14 +0900

    Release v0.4.2-7

--- params/version.go
+++ params/version.go
@@ -19,10 +19,10 @@ package params
 import "fmt"
 
 const (
-	ReleaseNum   = 6
+	ReleaseNum   = 7
 	VersionMajor = 0 // Major version component of the current release
 	VersionMinor = 4 // Minor version component of the current release
-	VersionPatch = 1 // Patch version component of the current release
+	VersionPatch = 2 // Patch version component of the current release
 )
 
 // Version holds the textual version string.
