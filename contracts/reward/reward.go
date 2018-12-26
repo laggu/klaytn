45ea459656b5e706679f529eca276c1489dc7ca2
Sangmin Seo (Sam) <sam.seo@groundx.xyz>
2018-12-26 13:09:46 +0900

    contracts: update copyright and license phrases (#801)
    For issue #138

--- contracts/reward/reward.go
+++ contracts/reward/reward.go
@@ -1,3 +1,20 @@
+// Copyright 2018 The go-klaytn Authors
+//
+// This file is part of the go-klaytn library.
+//
+// The go-klaytn library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-klaytn library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-klaytn library. If not, see <http://www.gnu.org/licenses/>.
+
 //go:generate abigen --sol contract/GXPReward.sol --pkg contract --out contract/GXPReward.go
 //go:generate abigen --sol contract/CommitteeReward.sol --pkg contract --out contract/CommitteeReward.go
 //go:generate abigen --sol contract/RNReward.sol --pkg contract --out contract/RNReward.go
