0c3a724868cf590c6cd7be2f21a96947f0382fc9
Jesse Jonghui Lee <jesse.lee@groundx.xyz>
2018-12-26 15:02:31 +0900

    Update a license for event package (#809)
    * Update a license for event package
    
    Signed-off-by: jesse.lee <jesse.lee@groundx.xyz>

--- event/subscription.go
+++ event/subscription.go
@@ -1,4 +1,5 @@
-// Copyright 2015 The go-ethereum Authors
+// Copyright 2018 go-klaytn Authors
+// Copyright 2016 The go-ethereum Authors
 // This file is part of the go-ethereum library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
@@ -13,6 +14,9 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is derived from event/subscription.go (2018/06/04).
+// Modified and improved for the go-klaytn development.
 
 package event
 
