167e0db1c0ee6bec5b7aecaecd3bbbb46950551f
Sangmin Seo (Sam) <sam.seo@groundx.xyz>
2018-12-26 10:11:36 +0900

    common: revert mclock/mclock.go to the forked version (#799)
    This patch changes the copyright statement and adds the package
    documents in common/mclock/mclock.go in order to make it the same as the
    originally forked version from go-ethereum.
    
    For issue #138

--- common/mclock/mclock.go
+++ common/mclock/mclock.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The go-ethereum Authors
+// Copyright 2016 The go-ethereum Authors
 // This file is part of the go-ethereum library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// package mclock is a wrapper for a monotonic clock source
 package mclock
 
 import (
