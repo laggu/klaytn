e8518a0f121c6b52d7017016c9ea5271b8873839
Sangmin Seo (Sam) <sam.seo@groundx.xyz>
2019-01-02 16:40:54 +0900

    consensus: fix a wrong origin path (#856)
    This patch fixes an incorrect origin path in consensus/protocol.go
    
    For issue #138

--- consensus/protocol.go
+++ consensus/protocol.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 //
-// This file is derived from quorum/consensus/istanbul/protocol.go (2018/06/04).
+// This file is derived from quorum/consensus/protocol.go (2018/06/04).
 // Modified and improved for the go-klaytn development.
 
 package consensus
