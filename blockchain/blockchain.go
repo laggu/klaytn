laggu/klaytn

commit 013d8bb1608ea831a6b398d9e913bc71642635ea

Removed unused error value relevant to the function `GetReceiptsInCache` (#848)
* Added error checking for the function `GetReceiptsInCache`

See: #831

* Modified GetReceiptsinCache instead of handling error

- removed unused return error type

* Removed the original return error value

- removed the unused error that is returned by GetReceiptsInCache

blockchain/blockchain.go
@@ -1206,11 +1206,11 @@ func (bc *BlockChain) GetTransactionInCache(hash common.Hash) (*types.Transactio
 	return txLookup.Tx, txLookup.BlockHash, txLookup.BlockIndex, txLookup.Index
 }
 
-func (bc *BlockChain) GetReceiptsInCache(blockHash common.Hash) (types.Receipts, error) {
+func (bc *BlockChain) GetReceiptsInCache(blockHash common.Hash) types.Receipts {
 	value, ok := bc.recentReceipts.Get(blockHash)
 	if !ok {
 		cacheGetRecentReceiptsMissMeter.Mark(1)
-		return nil, nil
+		return nil
 	}
 	cacheGetRecentReceiptsHitMeter.Mark(1)
 	items := value.([]*types.Receipt)
@@ -1218,7 +1218,7 @@ func (bc *BlockChain) GetReceiptsInCache(blockHash common.Hash) (types.Receipts,
 	for i, receipt := range items {
 		receipts[i] = receipt
 	}
-	return receipts, nil
+	return receipts
 }
 
 // InsertChain attempts to insert the given batch of blocks in to the canonical

consensus/istanbul/backend/api.go
@@ -314,7 +314,7 @@ func (api *APIExtension) GetBlockWithConsensusInfoByNumber(number *rpc.BlockNumb
 		return nil, errInternalError
 	}
 
-	receipts, err := b.GetReceiptsInCache(blockHash)
+	receipts := b.GetReceiptsInCache(blockHash)
 	if receipts == nil {
 		receipts = b.GetReceiptsByBlockHash(blockHash)
 	}
@@ -384,7 +384,7 @@ func (api *APIExtension) GetBlockWithConsensusInfoByHash(blockHash common.Hash) 
 		return nil, errInternalError
 	}
 
-	receipts, _ := b.GetReceiptsInCache(blockHash)
+	receipts := b.GetReceiptsInCache(blockHash)
 	if receipts == nil {
 		receipts = b.GetReceiptsByBlockHash(blockHash)
 	}

node/cn/api_backend.go
@@ -51,7 +51,7 @@ func (b *GxpAPIBackend) GetTransactionInCache(hash common.Hash) (*types.Transact
 }
 
 func (b *GxpAPIBackend) GetReceiptsInCache(blockHash common.Hash) (types.Receipts, error) {
-	return b.gxp.blockchain.GetReceiptsInCache(blockHash)
+	return b.gxp.blockchain.GetReceiptsInCache(blockHash), nil
 }
 
 func (b *GxpAPIBackend) ChainConfig() *params.ChainConfig {

commit f076e8323ace2323306fd1b2523cbb328ad52476

blockchain: hot fix for assignment mismatch (#869)
This is to fix the assignment mismatch caused by #848

blockchain/blockchain.go
@@ -696,7 +696,7 @@ func (bc *BlockChain) GetReceiptByTxHash(txHash common.Hash) *types.Receipt {
 		}
 	}
 
-	receipts, _ := bc.GetReceiptsInCache(blockHash)
+	receipts := bc.GetReceiptsInCache(blockHash)
 	if receipts == nil {
 		receipts = bc.GetReceiptsByBlockHash(blockHash)
 	}