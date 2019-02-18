a63dbcffd9d9042904e73adc2a50a813da4d0f6e
Sangmin Seo (Sam) <sam.seo@groundx.xyz>
2019-02-18 16:10:42 +0900

    Use slices for common node flags (#1333)
    To avoid managing the same flags in different node binaries, this patch
    creates slice variables holding common node/rpc flags.
    
    Related to #517

---
+++ cmd/utils/nodecmd/nodeflags.go
@@ -0,0 +1,130 @@
+// Copyright 2018 The klaytn Authors
+// Copyright 2016 The go-ethereum Authors
+// This file is part of go-ethereum.
+//
+// go-ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is derived from cmd/geth/main.go (2018/06/04).
+// Modified and improved for the klaytn development.
+
+package nodecmd
+
+import (
+	"github.com/ground-x/klaytn/cmd/utils"
+	"gopkg.in/urfave/cli.v1"
+)
+
+// Common flags that configure the node
+var CommonNodeFlags = []cli.Flag{
+	utils.IdentityFlag,
+	utils.UnlockedAccountFlag,
+	utils.PasswordFileFlag,
+	utils.BootnodesFlag,
+	utils.BootnodesV4Flag,
+	utils.DbTypeFlag,
+	utils.DataDirFlag,
+	utils.KeyStoreDirFlag,
+	utils.NoUSBFlag,
+	utils.DashboardEnabledFlag,
+	utils.EthashCacheDirFlag,
+	utils.EthashCachesInMemoryFlag,
+	utils.EthashCachesOnDiskFlag,
+	utils.EthashDatasetDirFlag,
+	utils.EthashDatasetsInMemoryFlag,
+	utils.EthashDatasetsOnDiskFlag,
+	utils.TxPoolNoLocalsFlag,
+	utils.TxPoolJournalFlag,
+	utils.TxPoolRejournalFlag,
+	utils.TxPoolPriceLimitFlag,
+	utils.TxPoolPriceBumpFlag,
+	utils.TxPoolAccountSlotsFlag,
+	utils.TxPoolGlobalSlotsFlag,
+	utils.TxPoolAccountQueueFlag,
+	utils.TxPoolGlobalQueueFlag,
+	utils.TxPoolLifetimeFlag,
+	utils.FastSyncFlag,
+	utils.LightModeFlag,
+	utils.SyncModeFlag,
+	utils.GCModeFlag,
+	utils.LightServFlag,
+	utils.LightPeersFlag,
+	utils.LightKDFFlag,
+	utils.PartitionedDBFlag,
+	utils.LevelDBCacheSizeFlag,
+	utils.DBParallelDBWriteFlag,
+	utils.TrieMemoryCacheSizeFlag,
+	utils.TrieCacheGenFlag,
+	utils.TrieBlockIntervalFlag,
+	utils.CacheTypeFlag,
+	utils.CacheScaleFlag,
+	utils.CacheUsageLevelFlag,
+	utils.MemorySizeFlag,
+	utils.ChildChainIndexingFlag,
+	utils.CacheWriteThroughFlag,
+	utils.ListenPortFlag,
+	utils.SubListenPortFlag,
+	utils.MultiChannelUseFlag,
+	utils.MaxPeersFlag,
+	utils.MaxPendingPeersFlag,
+	utils.CoinbaseFlag,
+	utils.RewardbaseFlag,
+	utils.RewardContractFlag,
+	utils.GasPriceFlag,
+	utils.MinerThreadsFlag,
+	utils.MiningEnabledFlag,
+	utils.TargetGasLimitFlag,
+	utils.NATFlag,
+	utils.NoDiscoverFlag,
+	utils.DiscoveryV5Flag,
+	utils.NetrestrictFlag,
+	utils.NodeKeyFileFlag,
+	utils.NodeKeyHexFlag,
+	utils.DeveloperFlag,
+	utils.DeveloperPeriodFlag,
+	utils.VMEnableDebugFlag,
+	utils.VMLogTargetFlag,
+	utils.NetworkIdFlag,
+	utils.RPCCORSDomainFlag,
+	utils.RPCVirtualHostsFlag,
+	utils.EthStatsURLFlag,
+	utils.MetricsEnabledFlag,
+	utils.PrometheusExporterFlag,
+	utils.PrometheusExporterPortFlag,
+	utils.FakePoWFlag,
+	utils.NoCompactionFlag,
+	utils.GpoBlocksFlag,
+	utils.GpoPercentileFlag,
+	utils.ExtraDataFlag,
+	utils.SrvTypeFlag,
+	utils.ChainAccountAddrFlag,
+	utils.AnchoringPeriodFlag,
+	utils.SentChainTxsLimit,
+	utils.BaobabFlag,
+	ConfigFileFlag,
+}
+
+// Common RPC flags
+var CommonRPCFlags = []cli.Flag{
+	utils.RPCEnabledFlag,
+	utils.RPCListenAddrFlag,
+	utils.RPCPortFlag,
+	utils.RPCApiFlag,
+	utils.WSEnabledFlag,
+	utils.WSListenAddrFlag,
+	utils.WSPortFlag,
+	utils.WSApiFlag,
+	utils.WSAllowedOriginsFlag,
+	utils.IPCDisabledFlag,
+	utils.IPCPathFlag,
+}
