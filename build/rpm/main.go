c0c78417e08d5468836ea77c335e040c858aaf51
Jesse Jonghui Lee <jesse.lee@groundx.xyz>
2019-02-26 10:57:02 +0900

    Refactoring RPM Package CI which support sperated klaytn binaries (#1483)
    * Refactoring RPM Package CI which support seperated klaytn binaries
    Support upgrade RPM

--- build/rpm/main.go
+++ build/rpm/main.go
@@ -6,13 +6,35 @@ import (
 	"github.com/ground-x/klaytn/params"
 	"github.com/urfave/cli"
 	"os"
+	"strings"
 	"text/template"
 )
+
+const (
+	CN = "kcn"
+	PN = "kpn"
+	EN = "ken"
+)
+
+type NodeInfo struct {
+	daemon  string
+	summary string
+}
+
+var NODE_TYPE = map[string]NodeInfo{
+	CN: {"kcnd", "kcnd is klaytn consensus node daemon"},
+	PN: {"kpnd", "kpnd is klaytn proxy node daemon"},
+	EN: {"kend", "kend is klaytn endpoint node daemon"},
+}
 
 type RpmSpec struct {
 	BuildNumber int
 	Version     string
 	Name        string
+	Summary     string
+	MakeTarget  string
+	ProgramName string // kcn, kpn, ken
+	DaemonName  string // kcnd, kpnd, kend
 }
 
 func (r RpmSpec) String() string {
@@ -34,13 +56,17 @@ func (r RpmSpec) String() string {
 func main() {
 	app := cli.NewApp()
 	app.Name = "klaytn_rpmtool"
-	app.Version = "0.1"
+	app.Version = "0.2"
 	app.Commands = []cli.Command{
 		{
 			Name:    "gen_spec",
 			Aliases: []string{"a"},
-			Usage:   "generate rpm sepc file",
+			Usage:   "generate rpm spec file",
 			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "node_type",
+					Usage: "Klaytn node type (cn, pn, en)",
+				},
 				cli.BoolFlag{
 					Name:  "devel",
 					Usage: "generate spec for devel version",
@@ -80,6 +106,15 @@ func main() {
 
 func genspec(c *cli.Context) error {
 	rpmSpec := new(RpmSpec)
+
+	nodeType := c.String("node_type")
+	if _, ok := NODE_TYPE[nodeType]; ok != true {
+		return fmt.Errorf("node_type[\"%s\"] is not supported. Use --node_type [kcn, kpn, ken]", nodeType)
+	}
+
+	rpmSpec.ProgramName = strings.ToLower(nodeType)
+	rpmSpec.DaemonName = NODE_TYPE[nodeType].daemon
+
 	if c.Bool("devel") {
 		buildNum := c.Int("build_num")
 		if buildNum == 0 {
@@ -87,11 +122,12 @@ func genspec(c *cli.Context) error {
 			os.Exit(1)
 		}
 		rpmSpec.BuildNumber = buildNum
-		rpmSpec.Name = "klaytn-devel"
+		rpmSpec.Name = NODE_TYPE[nodeType].daemon + "-devel"
 	} else {
 		rpmSpec.BuildNumber = params.ReleaseNum
-		rpmSpec.Name = "klaytn"
+		rpmSpec.Name = NODE_TYPE[nodeType].daemon
 	}
+	rpmSpec.Summary = NODE_TYPE[nodeType].summary
 	rpmSpec.Version = params.Version
 	fmt.Println(rpmSpec)
 	return nil
@@ -100,7 +136,7 @@ func genspec(c *cli.Context) error {
 var rpmSpecTemplate = `Name:               {{ .Name }}
 Version:            {{ .Version }}
 Release:            {{ .BuildNumber }}%{?dist}
-Summary:            the Klaytn package
+Summary:            {{ .Summary }}
 
 Group:              Application/blockchain
 License:            GNU
@@ -115,20 +151,21 @@ BuildRoot:          %(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXX
 %setup -q
 
 %build
-make klay
+make {{ .ProgramName }}
 
 %install
-mkdir -p $RPM_BUILD_ROOT/usr/local/bin
-mkdir -p $RPM_BUILD_ROOT/etc/klay/conf
-mkdir -p $RPM_BUILD_ROOT/etc/init.d/
-mkdir -p $RPM_BUILD_ROOT/var/log/klay
+mkdir -p $RPM_BUILD_ROOT/usr/bin
+mkdir -p $RPM_BUILD_ROOT/etc/{{ .DaemonName }}/conf
+mkdir -p $RPM_BUILD_ROOT/etc/init.d
+mkdir -p $RPM_BUILD_ROOT/var/log/{{ .DaemonName }}
 
-cp build/bin/klay $RPM_BUILD_ROOT/usr/local/bin/klay
-cp build/rpm/etc/init.d/klay $RPM_BUILD_ROOT/etc/init.d/klay
-cp build/rpm/etc/klay/conf/klay.conf $RPM_BUILD_ROOT/etc/klay/conf/klay.conf
+cp build/bin/{{ .ProgramName }} $RPM_BUILD_ROOT/usr/bin/{{ .ProgramName }}
+cp build/rpm/etc/init.d/{{ .DaemonName }} $RPM_BUILD_ROOT/etc/init.d/{{ .DaemonName }}
+cp build/rpm/etc/{{ .DaemonName }}/conf/{{ .DaemonName }}.conf $RPM_BUILD_ROOT/etc/{{ .DaemonName }}/conf/{{ .DaemonName }}.conf
 
 %files
-%attr(755, -, -) /usr/local/bin/klay
-%attr(644, -, -) /etc/klay/conf/klay.conf
-%attr(754, -, -) /etc/init.d/klay
+%attr(755, -, -) /usr/bin/{{ .ProgramName }}
+%attr(644, -, -) /etc/{{ .DaemonName }}/conf/{{ .DaemonName }}.conf
+%attr(754, -, -) /etc/init.d/{{ .DaemonName }}
+%config(noreplace) /etc/{{ .DaemonName }}/conf/{{ .DaemonName }}.conf
 `
