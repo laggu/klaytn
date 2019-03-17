3b4d10f8f5cf2565437e1e46954e866a3de87560
aidan <48199072+aidan-kwon@users.noreply.github.com>
2019-03-17 13:12:08 +0900

    Introduce TestAccountCreationMultiSigKeyBigThreshold  (#1856)
    * Introduce TestAccountCreationMultiSigKeyBigThreshold (#1763)

--- tests/account_keytype_test.go
+++ tests/account_keytype_test.go
@@ -108,3 +108,92 @@ func TestAccountCreationMaxMultiSigKey(t *testing.T) {
 		prof.PrintProfileInfo()
 	}
 }
+
+// TestAccountCreationMultiSigKeyBigThreshold tests multiSig account creation with abnormal threshold.
+// When a multisig account is created, a threshold value should be less or equal to the total weight of private keys.
+// If not, the account cannot creates any valid signatures.
+// The test creates a multisig account with a threshold (10) and the total weight (6). (failed case)
+func TestAccountCreationMultiSigKeyBigThreshold(t *testing.T) {
+	if testing.Verbose() {
+		enableLog()
+	}
+	prof := profile.NewProfiler()
+
+	// Initialize blockchain
+	start := time.Now()
+	bcdata, err := NewBCData(6, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_blockchain", time.Now().Sub(start))
+	defer bcdata.Shutdown()
+
+	// Initialize address-balance map for verification
+	start = time.Now()
+	accountMap := NewAccountMap()
+	if err := accountMap.Initialize(bcdata); err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_accountMap", time.Now().Sub(start))
+
+	// reservoir account
+	reservoir := &TestAccountType{
+		Addr:  *bcdata.addrs[0],
+		Keys:  []*ecdsa.PrivateKey{bcdata.privKeys[0]},
+		Nonce: uint64(0),
+	}
+
+	// multisig setting
+	threshold := uint(10)
+	weights := []uint{1, 2, 3}
+	multisigAddr := common.HexToAddress("0xbbfa38050bf3167c887c086758f448ce067ea8ea")
+	prvKeys := []string{
+		"a5c9a50938a089618167c9d67dbebc0deaffc3c76ddc6b40c2777ae594380000",
+		"a5c9a50938a089618167c9d67dbebc0deaffc3c76ddc6b40c2777ae594380001",
+		"a5c9a50938a089618167c9d67dbebc0deaffc3c76ddc6b40c2777ae594380002",
+	}
+
+	// multi-sig account
+	multisig, err := createMultisigAccount(threshold, weights, prvKeys, multisigAddr)
+
+	if testing.Verbose() {
+		fmt.Println("reservoirAddr = ", reservoir.Addr.String())
+		fmt.Println("multisigAddr = ", multisig.Addr.String())
+	}
+
+	signer := types.NewEIP155Signer(bcdata.bc.Config().ChainID)
+
+	// creates a multisig account with a threshold (10) and the total weight (6). (failed case)
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(1000000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            multisig.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: false,
+			types.TxValueKeyAccountKey:    multisig.AccKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		receipt, _, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, types.ReceiptStatusErrUnsatisfiableThreshold, receipt.Status)
+
+		reservoir.Nonce += 1
+	}
+
+	if testing.Verbose() {
+		prof.PrintProfileInfo()
+	}
+}
