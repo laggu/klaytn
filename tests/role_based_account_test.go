0c7ec7b00034270fab8324c4462790685ee3a66d
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2019-03-17 13:14:17 +0900

    Introduce TestAccountUpdateRoleBasedWrongLength (#1875)
    TestAccountUpdateRoleBasedWrongLength tests if the role-based key having wrong length.
    It tests the following scenario:
    1. Create an account colin using TxTypeAccountCreation.
    2. Update key to RoleBasedKey with four roles. It should fail.
    3. Update key to RoleBasedKey with zero role. It should fail.
    
    Signed-off-by: Junghyun Colin Kim <colin.kim@groundx.xyz>

--- tests/role_based_account_test.go
+++ tests/role_based_account_test.go
@@ -388,3 +388,128 @@ func TestAccountUpdateRoleBasedNil(t *testing.T) {
 		assert.Equal(t, types.ReceiptStatusErrAccountKeyNilUninitializable, receipt.Status)
 	}
 }
+
+// TestAccountUpdateRoleBasedWrongLength tests if the role-based key having wrong length.
+// It tests the following scenario:
+// 1. Create an account colin using TxTypeAccountCreation.
+// 2. Update key to RoleBasedKey with four roles. It should fail.
+// 3. Update key to RoleBasedKey with zero role. It should fail.
+func TestAccountUpdateRoleBasedWrongLength(t *testing.T) {
+	if testing.Verbose() {
+		enableLog()
+	}
+	prof := profile.NewProfiler()
+
+	// Initialize blockchain
+	start := time.Now()
+	bcdata, err := NewBCData(6, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_blockchain", time.Now().Sub(start))
+	defer bcdata.Shutdown()
+
+	// Initialize address-balance map for verification
+	start = time.Now()
+	accountMap := NewAccountMap()
+	if err := accountMap.Initialize(bcdata); err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_accountMap", time.Now().Sub(start))
+
+	// reservoir account
+	reservoir := &TestAccountType{
+		Addr:  *bcdata.addrs[0],
+		Keys:  []*ecdsa.PrivateKey{bcdata.privKeys[0]},
+		Nonce: uint64(0),
+	}
+
+	colin, err := createHumanReadableAccount("ed580f5bd71a2ee4dae5cb43e331b7d0318596e561e6add7844271ed94156b20", "colin")
+	assert.Equal(t, nil, err)
+
+	if testing.Verbose() {
+		fmt.Println("reservoirAddr = ", reservoir.Addr.String())
+		fmt.Println("colinAddr = ", colin.Addr.String())
+	}
+
+	signer := types.NewEIP155Signer(bcdata.bc.Config().ChainID)
+
+	// 1. Create an account colin using TxTypeAccountCreation.
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(1000000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            colin.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: true,
+			types.TxValueKeyAccountKey:    colin.AccKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		if err := bcdata.GenABlockWithTransactions(accountMap, txs, prof); err != nil {
+			t.Fatal(err)
+		}
+		reservoir.Nonce += 1
+	}
+
+	// 2. Update key to RoleBasedKey with four roles. It should fail.
+	{
+		keys := genTestKeys(4)
+		accKey := accountkey.NewAccountKeyRoleBasedWithValues(accountkey.AccountKeyRoleBased{
+			accountkey.NewAccountKeyPublicWithValue(&keys[0].PublicKey),
+			accountkey.NewAccountKeyPublicWithValue(&keys[1].PublicKey),
+			accountkey.NewAccountKeyPublicWithValue(&keys[2].PublicKey),
+			accountkey.NewAccountKeyPublicWithValue(&keys[3].PublicKey),
+		})
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:      colin.Nonce,
+			types.TxValueKeyFrom:       colin.Addr,
+			types.TxValueKeyGasLimit:   gasLimit,
+			types.TxValueKeyGasPrice:   gasPrice,
+			types.TxValueKeyAccountKey: accKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountUpdate, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, colin.Keys)
+		assert.Equal(t, nil, err)
+
+		receipt, _, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, types.ReceiptStatusErrLengthTooLong, receipt.Status)
+	}
+
+	// 3. Update key to RoleBasedKey with zero role. It should fail.
+	{
+		accKey := accountkey.NewAccountKeyRoleBasedWithValues(accountkey.AccountKeyRoleBased{})
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:      colin.Nonce,
+			types.TxValueKeyFrom:       colin.Addr,
+			types.TxValueKeyGasLimit:   gasLimit,
+			types.TxValueKeyGasPrice:   gasPrice,
+			types.TxValueKeyAccountKey: accKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountUpdate, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, colin.Keys)
+		assert.Equal(t, nil, err)
+
+		receipt, _, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, types.ReceiptStatusErrZeroLength, receipt.Status)
+	}
+}
