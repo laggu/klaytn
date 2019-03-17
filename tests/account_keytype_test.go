3fecb02b20c383982554a6282265f88dda4648a5
aidan <48199072+aidan-kwon@users.noreply.github.com>
2019-03-17 21:20:56 +0900

    Introduce TestAccountCreationMultiSigKeyDupPrvKeys
    * Introduce TestAccountCreationMultiSigKeyDupPrvKeys

--- tests/account_keytype_test.go
+++ tests/account_keytype_test.go
@@ -14,8 +14,8 @@ import (
 	"time"
 )
 
-// TestAccountCreationMaxMultiSigKey tests multiSig account creation with maximum signers.
-// A multiSig account supports maximum 10 different signers.
+// TestAccountCreationMaxMultiSigKey tests multiSig account creation with maximum private keys.
+// A multiSig account supports maximum 10 different private keys.
 // Create a multiSig account with 11 different private keys (more than 10 -> failed)
 func TestAccountCreationMaxMultiSigKey(t *testing.T) {
 	if testing.Verbose() {
@@ -75,7 +75,7 @@ func TestAccountCreationMaxMultiSigKey(t *testing.T) {
 
 	signer := types.NewEIP155Signer(bcdata.bc.Config().ChainID)
 
-	// Create a multiSig account with 11 different signers (more than 10 -> failed)
+	// Create a multiSig account with 11 different private keys (more than 10 -> failed)
 	{
 		var txs types.Transactions
 
@@ -490,6 +490,94 @@ func TestAccountCreationRoleBasedKeyInvalidNumKey(t *testing.T) {
 		receipt, _, err := applyTransaction(t, bcdata, tx)
 		assert.Equal(t, nil, err)
 		assert.Equal(t, types.ReceiptStatusErrZeroLength, receipt.Status)
+	}
+
+	if testing.Verbose() {
+		prof.PrintProfileInfo()
+	}
+}
+
+// TestAccountCreationMultiSigKeyDupPrvKeys tests multiSig account creation with duplicated private keys.
+// A multisig account has all different private keys, therefore account creation with duplicated private keys should be failed.
+// the case when two same private keys are used in creation processes.
+func TestAccountCreationMultiSigKeyDupPrvKeys(t *testing.T) {
+	if testing.Verbose() {
+		enableLog()
+	}
+	prof := profile.NewProfiler()
+
+	// Initialize blockchain
+	start := time.Now()
+	bcdata, err := NewBCData(6, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_blockchain", time.Now().Sub(start))
+	defer bcdata.Shutdown()
+
+	// Initialize address-balance map for verification
+	start = time.Now()
+	accountMap := NewAccountMap()
+	if err := accountMap.Initialize(bcdata); err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_accountMap", time.Now().Sub(start))
+
+	// reservoir account
+	reservoir := &TestAccountType{
+		Addr:  *bcdata.addrs[0],
+		Keys:  []*ecdsa.PrivateKey{bcdata.privKeys[0]},
+		Nonce: uint64(0),
+	}
+
+	// the case when two same private keys are used in creation processes.
+	threshold := uint(2)
+	weights := []uint{1, 1, 2}
+	multisigAddr := common.HexToAddress("0xbbfa38050bf3167c887c086758f448ce067ea8ea")
+	prvKeys := []string{
+		"a5c9a50938a089618167c9d67dbebc0deaffc3c76ddc6b40c2777ae594380000",
+		"a5c9a50938a089618167c9d67dbebc0deaffc3c76ddc6b40c2777ae594380001",
+		"a5c9a50938a089618167c9d67dbebc0deaffc3c76ddc6b40c2777ae594380001",
+	}
+
+	// multi-sig account
+	multisig, err := createMultisigAccount(threshold, weights, prvKeys, multisigAddr)
+
+	if testing.Verbose() {
+		fmt.Println("reservoirAddr = ", reservoir.Addr.String())
+		fmt.Println("multisigAddr = ", multisig.Addr.String())
+	}
+
+	signer := types.NewEIP155Signer(bcdata.bc.Config().ChainID)
+
+	// the case when two same private keys are used in creation processes.
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(1000000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            multisig.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: false,
+			types.TxValueKeyAccountKey:    multisig.AccKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		receipt, _, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, types.ReceiptStatusErrDuplicatedKey, receipt.Status)
+
+		reservoir.Nonce += 1
 	}
 
 	if testing.Verbose() {
