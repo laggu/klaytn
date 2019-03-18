c0b1b2c828a5d155752f79ff6560624a6e704794
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2019-03-18 11:09:10 +0900

    Introduce TestTransactionGas (#1889)
    TestTransactionGas checks gas calculation of base transaction types such as:
    1. LegacyTransaction
    2. TxTypeValueTransfer
    3. TxTypeValueTransferMemo with non-zero values.
    4. TxTypeValueTransferMemo with zero values.
    5. TxTypeAccountCreation
    6. TxTypeAccountUpdate
    7. TxTypeSmartContractDeploy
    8. TxTypeSmartContractExecution
    9. TxTypeCancel
    
    Related to #1829
    
    Signed-off-by: Junghyun Colin Kim <colin.kim@groundx.xyz>

---
+++ tests/tx_intrinsic_gas_test.go
@@ -0,0 +1,397 @@
+// Copyright 2018 The klaytn Authors
+// This file is part of the klaytn library.
+//
+// The klaytn library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The klaytn library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
+
+package tests
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ground-x/klaytn/accounts/abi"
+	"github.com/ground-x/klaytn/blockchain/types"
+	"github.com/ground-x/klaytn/blockchain/types/accountkey"
+	"github.com/ground-x/klaytn/common"
+	"github.com/ground-x/klaytn/common/profile"
+	"github.com/ground-x/klaytn/crypto"
+	"github.com/ground-x/klaytn/params"
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"strings"
+	"testing"
+	"time"
+)
+
+// TestTransactionGas checks gas calculation of base transaction types such as:
+// 1. LegacyTransaction
+// 2. TxTypeValueTransfer
+// 3. TxTypeValueTransferMemo with non-zero values.
+// 4. TxTypeValueTransferMemo with zero values.
+// 5. TxTypeAccountCreation
+// 6. TxTypeAccountUpdate
+// 7. TxTypeSmartContractDeploy
+// 8. TxTypeSmartContractExecution
+// 9. TxTypeCancel
+func TestTransactionGas(t *testing.T) {
+	if testing.Verbose() {
+		enableLog()
+	}
+	prof := profile.NewProfiler()
+
+	// Initialize blockchain
+	start := time.Now()
+	bcdata, err := NewBCData(6, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_blockchain", time.Now().Sub(start))
+	defer bcdata.Shutdown()
+
+	// Initialize address-balance map for verification
+	start = time.Now()
+	accountMap := NewAccountMap()
+	if err := accountMap.Initialize(bcdata); err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_accountMap", time.Now().Sub(start))
+
+	// reservoir account
+	reservoir := &TestAccountType{
+		Addr:  *bcdata.addrs[0],
+		Keys:  []*ecdsa.PrivateKey{bcdata.privKeys[0]},
+		Nonce: uint64(0),
+	}
+
+	// anonymous account
+	anon, err := createAnonymousAccount("98275a145bc1726eb0445433088f5f882f8a4a9499135239cfb4040e78991dab")
+	assert.Equal(t, nil, err)
+
+	// decoupled account
+	decoupled, err := createDecoupledAccount("c64f2cd1196e2a1791365b00c4bc07ab8f047b73152e4617c6ed06ac221a4b0c",
+		common.HexToAddress("0x75c3098be5e4b63fbac05838daaee378dd48098d"))
+	assert.Equal(t, nil, err)
+
+	colin, err := createHumanReadableAccount("ed580f5bd71a2ee4dae5cb43e331b7d0318596e561e6add7844271ed94156b20", "colin")
+	assert.Equal(t, nil, err)
+
+	contract, err := createHumanReadableAccount("ed34b0cf47a0021e9897760f0a904a69260c2f638e0bcc805facb745ec3ff9ab",
+		"contract")
+	assert.Equal(t, nil, err)
+
+	if testing.Verbose() {
+		fmt.Println("reservoirAddr = ", reservoir.Addr.String())
+		fmt.Println("anonAddr = ", anon.Addr.String())
+		fmt.Println("decoupledAddr = ", decoupled.Addr.String())
+		fmt.Println("colinAddr = ", colin.Addr.String())
+	}
+
+	signer := types.NewEIP155Signer(bcdata.bc.Config().ChainID)
+
+	// Preparing step. Create an account decoupled.
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(1000000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            decoupled.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: false,
+			types.TxValueKeyAccountKey:    decoupled.AccKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		if err := bcdata.GenABlockWithTransactions(accountMap, txs, prof); err != nil {
+			t.Fatal(err)
+		}
+		reservoir.Nonce += 1
+	}
+
+	// Preparing step. Deploy a smart contract.
+	var abiStr string
+	var code string
+
+	if isCompilerAvailable() {
+		filename := string("../contracts/reward/contract/KlaytnReward.sol")
+		codes, abistrings := compileSolidity(filename)
+		code = codes[0]
+		abiStr = abistrings[0]
+	} else {
+		// Falling back to use compiled code.
+		code = "0x608060405234801561001057600080fd5b506101de806100206000396000f3006080604052600436106100615763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416631a39d8ef81146100805780636353586b146100a757806370a08231146100ca578063fd6b7ef8146100f8575b3360009081526001602052604081208054349081019091558154019055005b34801561008c57600080fd5b5061009561010d565b60408051918252519081900360200190f35b6100c873ffffffffffffffffffffffffffffffffffffffff60043516610113565b005b3480156100d657600080fd5b5061009573ffffffffffffffffffffffffffffffffffffffff60043516610147565b34801561010457600080fd5b506100c8610159565b60005481565b73ffffffffffffffffffffffffffffffffffffffff1660009081526001602052604081208054349081019091558154019055565b60016020526000908152604090205481565b336000908152600160205260408120805490829055908111156101af57604051339082156108fc029083906000818181858888f193505050501561019c576101af565b3360009081526001602052604090208190555b505600a165627a7a72305820627ca46bb09478a015762806cc00c431230501118c7c26c30ac58c4e09e51c4f0029"
+		abiStr = `[{"constant":true,"inputs":[],"name":"totalAmount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"}],"name":"reward","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"safeWithdrawal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]`
+	}
+
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(0)
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            contract.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: true,
+			types.TxValueKeyData:          common.FromHex(code),
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeSmartContractDeploy, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		if err := bcdata.GenABlockWithTransactions(accountMap, txs, prof); err != nil {
+			t.Fatal(err)
+		}
+		reservoir.Nonce += 1
+	}
+
+	// 1. LegacyTransaction
+	{
+		amount := new(big.Int).SetUint64(100000000000)
+		tx := types.NewTransaction(reservoir.Nonce,
+			anon.Addr, amount, gasLimit, gasPrice, []byte{})
+
+		err := tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		assert.Equal(t, params.TxGas, gas)
+	}
+
+	// 2. TxTypeValueTransfer
+	{
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    reservoir.Nonce,
+			types.TxValueKeyFrom:     reservoir.Addr,
+			types.TxValueKeyTo:       reservoir.Addr,
+			types.TxValueKeyAmount:   big.NewInt(100000), // smaller than total amount
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeValueTransfer, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		assert.Equal(t, params.TxGas, gas)
+	}
+
+	// 3. TxTypeValueTransferMemo with non-zero values.
+	{
+		data := []byte{1, 2, 3, 4}
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    reservoir.Nonce,
+			types.TxValueKeyFrom:     reservoir.Addr,
+			types.TxValueKeyTo:       reservoir.Addr,
+			types.TxValueKeyAmount:   big.NewInt(100000), // smaller than total amount
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+			types.TxValueKeyData:     data,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeValueTransferMemo, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		assert.Equal(t, params.TxGas+uint64(len(data))*params.TxDataNonZeroGas, gas)
+	}
+
+	// 4. TxTypeValueTransferMemo with zero values.
+	{
+		data := []byte{0, 0, 0, 0}
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    reservoir.Nonce,
+			types.TxValueKeyFrom:     reservoir.Addr,
+			types.TxValueKeyTo:       reservoir.Addr,
+			types.TxValueKeyAmount:   big.NewInt(100000), // smaller than total amount
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+			types.TxValueKeyData:     data,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeValueTransferMemo, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		assert.Equal(t, params.TxGas+uint64(len(data))*params.TxDataZeroGas, gas)
+	}
+
+	// 5. TxTypeAccountCreation
+	{
+		amount := new(big.Int).SetUint64(1000000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            colin.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: true,
+			types.TxValueKeyAccountKey:    colin.AccKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		assert.Equal(t, params.TxGasAccountCreation+params.TxAccountCreationGasDefault+uint64(len(colin.Keys))*params.TxAccountCreationGasPerKey, gas)
+	}
+
+	// 6. TxTypeAccountUpdate
+	{
+		newKey, err := crypto.HexToECDSA("41bd2b972564206658eab115f26ff4db617e6eb39c81a557adc18d8305d2f867")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:      decoupled.Nonce,
+			types.TxValueKeyFrom:       decoupled.Addr,
+			types.TxValueKeyGasLimit:   gasLimit,
+			types.TxValueKeyGasPrice:   gasPrice,
+			types.TxValueKeyAccountKey: accountkey.NewAccountKeyPublicWithValue(&newKey.PublicKey),
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountUpdate, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, decoupled.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		numKeys := uint64(len(decoupled.Keys))
+		validationGas := params.TxValidationGasDefault + numKeys*params.TxValidationGasPerKey
+
+		assert.Equal(t, params.TxGasAccountUpdate+validationGas+params.TxAccountCreationGasDefault+numKeys*params.TxAccountCreationGasPerKey, gas)
+	}
+
+	// 7. TxTypeSmartContractDeploy
+	{
+		amount := new(big.Int).SetUint64(0)
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            common.HexToAddress("12345678"),
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: true,
+			types.TxValueKeyData:          common.FromHex(code),
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeSmartContractDeploy, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		intrinsicGas, err := types.IntrinsicGas(common.FromHex(code), true, true)
+		assert.Equal(t, nil, err)
+
+		executionGas := uint64(0x3B9982BF)
+		assert.Equal(t, intrinsicGas+executionGas, gas)
+	}
+
+	// 8. TxTypeSmartContractExecution
+	{
+		amountToSend := new(big.Int).SetUint64(10)
+		abii, err := abi.JSON(strings.NewReader(string(abiStr)))
+		assert.Equal(t, nil, err)
+
+		data, err := abii.Pack("reward", reservoir.Addr)
+		assert.Equal(t, nil, err)
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    reservoir.Nonce,
+			types.TxValueKeyFrom:     reservoir.Addr,
+			types.TxValueKeyTo:       contract.Addr,
+			types.TxValueKeyAmount:   amountToSend,
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+			types.TxValueKeyData:     data,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeSmartContractExecution, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		intrinsicGas, err := types.IntrinsicGas(data, false, true)
+		assert.Equal(t, nil, err)
+
+		executionGas := uint64(0x9ec4)
+		assert.Equal(t, intrinsicGas+executionGas, gas)
+	}
+
+	// 9. TxTypeCancel
+	{
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    reservoir.Nonce,
+			types.TxValueKeyFrom:     reservoir.Addr,
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeCancel, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		_, gas, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+
+		assert.Equal(t, params.TxGasCancel, gas)
+	}
+
+}
