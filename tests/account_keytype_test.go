966d3d1c3e908b0e33418277efffd540563b4bcd
aidan <48199072+aidan-kwon@users.noreply.github.com>
2019-03-19 09:19:33 +0900

    Introduce TestAccountTransferWithRoleBasedTxKey (#1926)

--- tests/account_keytype_test.go
+++ tests/account_keytype_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"crypto/ecdsa"
 	"fmt"
+	"github.com/ground-x/klaytn/accounts/abi"
 	"github.com/ground-x/klaytn/blockchain/types"
 	"github.com/ground-x/klaytn/blockchain/types/accountkey"
 	"github.com/ground-x/klaytn/common"
@@ -13,6 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"math/big"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
@@ -1198,6 +1200,263 @@ func TestAccountUpdateWithRoleBasedKey(t *testing.T) {
 		assert.Equal(t, types.ErrInvalidSigSender, err)
 
 		anon.Nonce += 1
+	}
+
+	if testing.Verbose() {
+		prof.PrintProfileInfo()
+	}
+}
+
+// TestAccountTransferWithRoleBasedTxKey tests Txs signed by a RoleTransaction key contained in a role-based key.
+// A role-based key contains three types of sub-keys: RoleTransaction, RoleAccountUpdate, RoleFeePayer.
+// A RoleTransaction key can sign for any transactions except accountUpdate Txs and FeeDelegating Txs.
+// 1. create an account with a role-based key.
+// 2. TxTypeValueTransfer signed by a RoleTransaction key.
+// 3. TxTypeAccountCreation signed by a RoleTransaction key.
+// 4. TxTypeSmartContractDeploy signed by a RoleTransaction key.
+// 5. TxTypeSmartContractExecution signed by a RoleTransaction key.
+// 6. TxTypeCancel signed by a RoleTransaction key.
+// 7. TxTypeChainDataAnchoringTransaction signed by a RoleTransaction key.
+func TestAccountTransferWithRoleBasedTxKey(t *testing.T) {
+	if testing.Verbose() {
+		enableLog()
+	}
+	prof := profile.NewProfiler()
+
+	// Initialize blockchain
+	start := time.Now()
+	bcdata, err := NewBCData(6, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_blockchain", time.Now().Sub(start))
+	defer bcdata.Shutdown()
+
+	gasPrice := new(big.Int).SetUint64(0)
+	gasLimit := uint64(2500000000)
+
+	// Initialize address-balance map for verification
+	start = time.Now()
+	accountMap := NewAccountMap()
+	if err := accountMap.Initialize(bcdata); err != nil {
+		t.Fatal(err)
+	}
+	prof.Profile("main_init_accountMap", time.Now().Sub(start))
+
+	// reservoir account
+	reservoir := &TestAccountType{
+		Addr:  *bcdata.addrs[0],
+		Keys:  []*ecdsa.PrivateKey{bcdata.privKeys[0]},
+		Nonce: uint64(0),
+	}
+
+	// main account with a role-based key
+	roleBased, err := createAnonymousAccount("98275a145bc1726eb0445433088f5f882f8a4a9499135239cfb4040e78991dab")
+	assert.Equal(t, nil, err)
+
+	// anonymous account
+	anon, err := createAnonymousAccount("98275a145bc1726eb0445433088f5f882f8a4a9499135239cfb4040e78990000")
+	assert.Equal(t, nil, err)
+
+	// smart contact account
+	contract, err := createHumanReadableAccount("ed34b0cf47a0021e9897760f0a904a69260c2f638e0bcc805facb745ec3ff9ab",
+		"contract")
+	assert.Equal(t, nil, err)
+
+	// Smart contract used to test TxTypeSmartContractDeploy, TxTypeSmartContractExecution transactions
+	var code string
+	var abiStr string
+
+	if isCompilerAvailable() {
+		filename := string("../contracts/reward/contract/KlaytnReward.sol")
+		codes, abistrings := compileSolidity(filename)
+		code = codes[0]
+		abiStr = abistrings[0]
+	} else {
+		// Falling back to use compiled code.
+		code = "0x608060405234801561001057600080fd5b506101de806100206000396000f3006080604052600436106100615763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416631a39d8ef81146100805780636353586b146100a757806370a08231146100ca578063fd6b7ef8146100f8575b3360009081526001602052604081208054349081019091558154019055005b34801561008c57600080fd5b5061009561010d565b60408051918252519081900360200190f35b6100c873ffffffffffffffffffffffffffffffffffffffff60043516610113565b005b3480156100d657600080fd5b5061009573ffffffffffffffffffffffffffffffffffffffff60043516610147565b34801561010457600080fd5b506100c8610159565b60005481565b73ffffffffffffffffffffffffffffffffffffffff1660009081526001602052604081208054349081019091558154019055565b60016020526000908152604090205481565b336000908152600160205260408120805490829055908111156101af57604051339082156108fc029083906000818181858888f193505050501561019c576101af565b3360009081526001602052604090208190555b505600a165627a7a72305820627ca46bb09478a015762806cc00c431230501118c7c26c30ac58c4e09e51c4f0029"
+		abiStr = `[{"constant":true,"inputs":[],"name":"totalAmount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"}],"name":"reward","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"safeWithdrawal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]`
+	}
+
+	// generate a role-based key
+	keys := genTestKeys(3)
+	roleKey := accountkey.NewAccountKeyRoleBasedWithValues(accountkey.AccountKeyRoleBased{
+		accountkey.NewAccountKeyPublicWithValue(&keys[0].PublicKey),
+		accountkey.NewAccountKeyPublicWithValue(&keys[1].PublicKey),
+		accountkey.NewAccountKeyPublicWithValue(&keys[2].PublicKey),
+	})
+
+	// a RoleTransaction key for signing
+	txKey := []*ecdsa.PrivateKey{keys[accountkey.RoleTransaction]}
+
+	if testing.Verbose() {
+		fmt.Println("reservoirAddr = ", reservoir.Addr.String())
+		fmt.Println("roleBasedAddr = ", roleBased.Addr.String())
+	}
+
+	signer := types.NewEIP155Signer(bcdata.bc.Config().ChainID)
+
+	// 1. create an account with a role-based key.
+	{
+		var txs types.Transactions
+		amount := new(big.Int).SetUint64(1000000000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         reservoir.Nonce,
+			types.TxValueKeyFrom:          reservoir.Addr,
+			types.TxValueKeyTo:            roleBased.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: false,
+			types.TxValueKeyAccountKey:    roleKey,
+		}
+
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		err = tx.SignWithKeys(signer, reservoir.Keys)
+		assert.Equal(t, nil, err)
+
+		if err := bcdata.GenABlockWithTransactions(accountMap, txs, prof); err != nil {
+			t.Fatal(err)
+		}
+
+		reservoir.Nonce += 1
+	}
+
+	// 2. TxTypeValueTransfer signed by a RoleTransaction key.
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(1000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    roleBased.Nonce,
+			types.TxValueKeyFrom:     roleBased.Addr,
+			types.TxValueKeyTo:       reservoir.Addr,
+			types.TxValueKeyAmount:   amount,
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeValueTransfer, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, txKey)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		err = bcdata.GenABlockWithTransactions(accountMap, txs, prof)
+		assert.Equal(t, nil, err)
+
+		roleBased.Nonce += 1
+	}
+
+	// 3. TxTypeAccountCreation signed by a RoleTransaction key.
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(1000000000)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         roleBased.Nonce,
+			types.TxValueKeyFrom:          roleBased.Addr,
+			types.TxValueKeyTo:            anon.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: false,
+			types.TxValueKeyAccountKey:    anon.AccKey,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeAccountCreation, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, txKey)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		err = bcdata.GenABlockWithTransactions(accountMap, txs, prof)
+		assert.Equal(t, nil, err)
+
+		roleBased.Nonce += 1
+	}
+
+	// 4. TxTypeSmartContractDeploy signed by a RoleTransaction key.
+	{
+		var txs types.Transactions
+
+		amount := new(big.Int).SetUint64(0)
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:         roleBased.Nonce,
+			types.TxValueKeyFrom:          roleBased.Addr,
+			types.TxValueKeyTo:            contract.Addr,
+			types.TxValueKeyAmount:        amount,
+			types.TxValueKeyGasLimit:      gasLimit,
+			types.TxValueKeyGasPrice:      gasPrice,
+			types.TxValueKeyHumanReadable: true,
+			types.TxValueKeyData:          common.FromHex(code),
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeSmartContractDeploy, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, txKey)
+		assert.Equal(t, nil, err)
+
+		txs = append(txs, tx)
+
+		err = bcdata.GenABlockWithTransactions(accountMap, txs, prof)
+		assert.Equal(t, nil, err)
+
+		roleBased.Nonce += 1
+	}
+
+	// 5. TxTypeSmartContractExecution signed by a RoleTransaction key.
+	{
+		amount := new(big.Int).SetUint64(0)
+		abii, err := abi.JSON(strings.NewReader(string(abiStr)))
+		assert.Equal(t, nil, err)
+
+		data, err := abii.Pack("reward", roleBased.Addr)
+		assert.Equal(t, nil, err)
+
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    roleBased.Nonce,
+			types.TxValueKeyFrom:     roleBased.Addr,
+			types.TxValueKeyTo:       contract.Addr,
+			types.TxValueKeyAmount:   amount,
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+			types.TxValueKeyData:     data,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeSmartContractExecution, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, txKey)
+		assert.Equal(t, nil, err)
+
+		receipt, _, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
+	}
+
+	// 6. TxTypeCancel signed by a RoleTransaction key.
+	{
+		values := map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:    roleBased.Nonce,
+			types.TxValueKeyFrom:     roleBased.Addr,
+			types.TxValueKeyGasLimit: gasLimit,
+			types.TxValueKeyGasPrice: gasPrice,
+		}
+		tx, err := types.NewTransactionWithMap(types.TxTypeCancel, values)
+		assert.Equal(t, nil, err)
+
+		err = tx.SignWithKeys(signer, txKey)
+		assert.Equal(t, nil, err)
+
+		receipt, _, err := applyTransaction(t, bcdata, tx)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
 	}
 
 	if testing.Verbose() {
