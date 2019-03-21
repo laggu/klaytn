aa8351f0143d3288786012610254e5e59fa4f713
Ethan(Kyungup Kim) <kyungup@gmail.com>
2019-03-21 15:23:29 +0900

    Replace "enode" to "kni" in nodeinfo API (#1971)

--- networks/p2p/server.go
+++ networks/p2p/server.go
@@ -1721,10 +1721,10 @@ func (srv *BaseServer) runPeer(p *Peer) {
 
 // NodeInfo represents a short summary of the information known about the host.
 type NodeInfo struct {
-	ID    string `json:"id"`    // Unique node identifier (also the encryption key)
-	Name  string `json:"name"`  // Name of the node, including client type, version, OS, custom data
-	Enode string `json:"enode"` // Enode URL for adding this peer from remote peers
-	IP    string `json:"ip"`    // IP address of the node
+	ID    string `json:"id"`   // Unique node identifier (also the encryption key)
+	Name  string `json:"name"` // Name of the node, including client type, version, OS, custom data
+	Enode string `json:"kni"`  // Enode URL for adding this peer from remote peers
+	IP    string `json:"ip"`   // IP address of the node
 	Ports struct {
 		Discovery int `json:"discovery"` // UDP listening port for discovery protocol
 		Listener  int `json:"listener"`  // TCP listening port for RLPx
