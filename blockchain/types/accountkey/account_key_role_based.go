5a727450f4b8735c1637ef2e7014a494bb8d0e8a
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2019-03-28 09:11:13 +0900

    Revise RLP serialization of role-based keys for caver.js (#2186)
    Since caver.js has different RLP serialization method, it cannot
    support current mechanism of role-based keys.
    To support both caver.js and Klaytn written in go,
    Klaytn should not override EncodeRLP() directly.
    
    Signed-off-by: Junghyun Colin Kim <colin.kim@groundx.xyz>

--- blockchain/types/accountkey/account_key_role_based.go
+++ blockchain/types/accountkey/account_key_role_based.go
@@ -93,24 +93,31 @@ func (a *AccountKeyRoleBased) Equal(b AccountKey) bool {
 }
 
 func (a *AccountKeyRoleBased) EncodeRLP(w io.Writer) error {
-	serializers := make([]*AccountKeySerializer, len(*a))
+	enc := make([][]byte, len(*a))
 
 	for i, k := range *a {
-		serializers[i] = NewAccountKeySerializerWithAccountKey(k)
+		enc[i], _ = rlp.EncodeToBytes(NewAccountKeySerializerWithAccountKey(k))
 	}
 
-	return rlp.Encode(w, serializers)
+	return rlp.Encode(w, enc)
 }
 
 func (a *AccountKeyRoleBased) DecodeRLP(s *rlp.Stream) error {
-	serializers := []*AccountKeySerializer{}
-	if err := s.Decode(&serializers); err != nil {
+	enc := [][]byte{}
+	if err := s.Decode(&enc); err != nil {
 		return err
 	}
-	*a = make(AccountKeyRoleBased, len(serializers))
-	for i, s := range serializers {
-		(*a)[i] = s.key
+
+	keys := make([]AccountKey, len(enc))
+	for i, b := range enc {
+		serializer := NewAccountKeySerializer()
+		if err := rlp.DecodeBytes(b, &serializer); err != nil {
+			return err
+		}
+		keys[i] = serializer.key
 	}
+
+	*a = (AccountKeyRoleBased)(keys)
 
 	return nil
 }
