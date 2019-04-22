0fd9bffcc89e418d00c83428479b68c16415c91d
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2019-04-22 15:15:07 +0900

    Refine JSON output of TxInternalDataFeeDelegatedValueTransferMemo (#2601)
    Related to #2555

--- blockchain/types/tx_internal_data_fee_delegated_value_transfer_memo.go
+++ blockchain/types/tx_internal_data_fee_delegated_value_transfer_memo.go
@@ -19,6 +19,7 @@ package types
 import (
 	"bytes"
 	"crypto/ecdsa"
+	"encoding/json"
 	"fmt"
 	"github.com/ground-x/klaytn/blockchain/types/accountkey"
 	"github.com/ground-x/klaytn/common"
@@ -45,6 +46,22 @@ type TxInternalDataFeeDelegatedValueTransferMemo struct {
 
 	// This is only used when marshaling to JSON.
 	Hash *common.Hash `json:"hash" rlp:"-"`
+}
+
+type TxInternalDataFeeDelegatedValueTransferMemoJSON struct {
+	Type               TxType           `json:"typeInt"`
+	TypeStr            string           `json:"type"`
+	AccountNonce       hexutil.Uint64   `json:"nonce"`
+	Price              *hexutil.Big     `json:"gasPrice"`
+	GasLimit           hexutil.Uint64   `json:"gas"`
+	Recipient          common.Address   `json:"to"`
+	Amount             *hexutil.Big     `json:"value"`
+	From               common.Address   `json:"from"`
+	Payload            hexutil.Bytes    `json:"input"`
+	TxSignatures       TxSignaturesJSON `json:"signatures"`
+	FeePayer           common.Address   `json:"feePayer"`
+	FeePayerSignatures TxSignaturesJSON `json:"feePayerSignatures"`
+	Hash               *common.Hash     `json:"hash"`
 }
 
 func newTxInternalDataFeeDelegatedValueTransferMemo() *TxInternalDataFeeDelegatedValueTransferMemo {
@@ -317,13 +334,55 @@ func (t *TxInternalDataFeeDelegatedValueTransferMemo) Execute(sender ContractRef
 
 func (t *TxInternalDataFeeDelegatedValueTransferMemo) MakeRPCOutput() map[string]interface{} {
 	return map[string]interface{}{
-		"type":     t.Type().String(),
-		"gas":      hexutil.Uint64(t.GasLimit),
-		"gasPrice": (*hexutil.Big)(t.Price),
-		"input":    hexutil.Bytes(t.Payload),
-		"nonce":    hexutil.Uint64(t.AccountNonce),
-		"to":       t.Recipient,
-		"value":    (*hexutil.Big)(t.Amount),
-		"feePayer": t.FeePayer,
+		"typeInt":            t.Type(),
+		"type":               t.Type().String(),
+		"gas":                hexutil.Uint64(t.GasLimit),
+		"gasPrice":           (*hexutil.Big)(t.Price),
+		"input":              hexutil.Bytes(t.Payload),
+		"nonce":              hexutil.Uint64(t.AccountNonce),
+		"to":                 t.Recipient,
+		"value":              (*hexutil.Big)(t.Amount),
+		"signatures":         t.TxSignatures.ToJSON(),
+		"feePayer":           t.FeePayer,
+		"feePayerSignatures": t.FeePayerSignatures.ToJSON(),
 	}
+}
+
+func (t *TxInternalDataFeeDelegatedValueTransferMemo) MarshalJSON() ([]byte, error) {
+	return json.Marshal(TxInternalDataFeeDelegatedValueTransferMemoJSON{
+		t.Type(),
+		t.Type().String(),
+		(hexutil.Uint64)(t.AccountNonce),
+		(*hexutil.Big)(t.Price),
+		(hexutil.Uint64)(t.GasLimit),
+		t.Recipient,
+		(*hexutil.Big)(t.Amount),
+		t.From,
+		t.Payload,
+		t.TxSignatures.ToJSON(),
+		t.FeePayer,
+		t.FeePayerSignatures.ToJSON(),
+		t.Hash,
+	})
+}
+
+func (t *TxInternalDataFeeDelegatedValueTransferMemo) UnmarshalJSON(b []byte) error {
+	js := &TxInternalDataFeeDelegatedValueTransferMemoJSON{}
+	if err := json.Unmarshal(b, js); err != nil {
+		return err
+	}
+
+	t.AccountNonce = uint64(js.AccountNonce)
+	t.Price = (*big.Int)(js.Price)
+	t.GasLimit = uint64(js.GasLimit)
+	t.Recipient = js.Recipient
+	t.Amount = (*big.Int)(js.Amount)
+	t.From = js.From
+	t.Payload = js.Payload
+	t.TxSignatures = js.TxSignatures.ToTxSignatures()
+	t.FeePayer = js.FeePayer
+	t.FeePayerSignatures = js.FeePayerSignatures.ToTxSignatures()
+	t.Hash = js.Hash
+
+	return nil
 }
