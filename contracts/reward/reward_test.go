e4d655b00fe77d3eb0af8e3e9701d14e68decf8e
Kuwon Ra(Sebastian) <sebastian.ra@groundx.xyz>
2019-04-25 19:31:46 +0900

    reward test - distribute reward and staking info (#2657)
    * add test PoC & KIR reward distribute
    
    * add test stakinginfo cache get
    
    * update poc & kir reward test
    
    * calculate minimum block number in stakingInfoCache

--- contracts/reward/reward_test.go
+++ contracts/reward/reward_test.go
@@ -172,6 +172,47 @@ func TestBlockRewardWithDeferredTxFeeEnabled(t *testing.T) {
 	}
 }
 
+func TestPocKirRewardDistribute(t *testing.T) {
+	allocBlockRewardCache()
+
+	accounts := newTestAccounts()
+	header := &types.Header{Number: big.NewInt(0)}
+	proposerAddr := addr1
+	header.Rewardbase = proposerAddr
+	header.GasUsed = uint64(100000)
+	mintingAmount := big.NewInt(int64(1000000000))
+
+	// chain config
+	config := &params.ChainConfig{Istanbul: governance.GetDefaultIstanbulConfig(), Governance: governance.GetDefaultGovernanceConfig(params.UseIstanbul)}
+	config.Governance.Reward.MintingAmount = mintingAmount
+	config.Governance.Reward.Ratio = "70/20/10"
+
+	pocAddr := common.StringToAddress("1111111111111111111111111111111111111111")
+	kirAddr := common.StringToAddress("2222222222222222222222222222222222222222")
+
+	distributeBlockReward(accounts, header, big.NewInt(0), pocAddr, kirAddr, config)
+
+	cnBalance := accounts.GetBalance(proposerAddr)
+	pocBalance := accounts.GetBalance(pocAddr)
+	kirBalance := accounts.GetBalance(kirAddr)
+
+	expectedKirBalance := big.NewInt(0).Div(mintingAmount, big.NewInt(10))     // 10%
+	expectedPocBalance := big.NewInt(0).Mul(expectedKirBalance, big.NewInt(2)) // 20%
+	expectedCnBalance := big.NewInt(0).Mul(expectedKirBalance, big.NewInt(7))  // 70%
+
+	if expectedCnBalance.Cmp(cnBalance) != 0 || pocBalance.Cmp(expectedPocBalance) != 0 || kirBalance.Cmp(expectedKirBalance) != 0 {
+		t.Errorf("balances are calculated incorrectly. CN Balance : %v, PoC Balance : %v, KIR Balance : %v, ratio : %v",
+			cnBalance, pocBalance, kirBalance, config.Governance.Reward.Ratio)
+	}
+
+	totalBalance := big.NewInt(0).Add(cnBalance, pocBalance)
+	totalBalance = big.NewInt(0).Add(totalBalance, kirBalance)
+
+	if mintingAmount.Cmp(totalBalance) != 0 {
+		t.Errorf("The sum of balance is diffrent from mintingAmount. totalBalance : %v, mintingAmount : %v", totalBalance, mintingAmount)
+	}
+}
+
 // TestBlockRewardWithCustomRewardRatio tests DistributeBlockReward with reward ratio defined in params package.
 func TestBlockRewardWithCustomRewardRatio(t *testing.T) {
 	// 1. DefaultGovernance + header.GasUsed + DeferredTxFee True + params.DefaultMintedKLAY + DefaultCNRatio/DefaultKIRRewardRatio/DefaultPocRewardRatio
@@ -203,5 +244,76 @@ func TestBlockRewardWithCustomRewardRatio(t *testing.T) {
 		expectedBalance := tmpInt.Add(tmpInt, config.Governance.Reward.MintingAmount)
 
 		assert.Equal(t, expectedBalance, balance)
+	}
+}
+
+func TestStakingInfoCache_Add(t *testing.T) {
+	initStakingCache()
+
+	// test cache limit
+	for i := 1; i <= 10; i++ {
+		testStakingInfo, _ := newEmptyStakingInfo(nil, uint64(i))
+		stakingCache.add(testStakingInfo)
+
+		if len(stakingCache.cells) > maxStakingCache {
+			t.Errorf("over the max limit of staking cache. Current Len : %v, MaxStakingCache : %v", len(stakingCache.cells), maxStakingCache)
+		}
+	}
+
+	// test adding same block number
+	initStakingCache()
+	testStakingInfo1, _ := newEmptyStakingInfo(nil, uint64(1))
+	testStakingInfo2, _ := newEmptyStakingInfo(nil, uint64(1))
+	stakingCache.add(testStakingInfo1)
+	stakingCache.add(testStakingInfo2)
+
+	if len(stakingCache.cells) > 1 {
+		t.Errorf("StakingInfo with Same block number is saved to the cache stakingCache. result : %v, expected : %v ", len(stakingCache.cells), maxStakingCache)
+	}
+
+	// test minBlockNum
+	initStakingCache()
+	for i := 1; i < 5; i++ {
+		testStakingInfo, _ := newEmptyStakingInfo(nil, uint64(i))
+		stakingCache.add(testStakingInfo)
+	}
+
+	testStakingInfo, _ := newEmptyStakingInfo(nil, uint64(5))
+	stakingCache.add(testStakingInfo) // blockNum 1 should be deleted
+	if stakingCache.minBlockNum != 2 {
+		t.Errorf("minBlockNum of staking cache is diffrent from expected blocknum. result : %v, expected : %v", stakingCache.minBlockNum, 2)
+	}
+
+	testStakingInfo, _ = newEmptyStakingInfo(nil, uint64(6))
+	stakingCache.add(testStakingInfo) // blockNum 2 should be deleted
+	if stakingCache.minBlockNum != 3 {
+		t.Errorf("minBlockNum of staking cache is diffrent from expected blocknum. result : %v, expected : %v", stakingCache.minBlockNum, 3)
+	}
+}
+
+func TestStakingInfoCache_Get(t *testing.T) {
+	initStakingCache()
+
+	for i := 1; i <= 4; i++ {
+		testStakingInfo, _ := newEmptyStakingInfo(nil, uint64(i))
+		stakingCache.add(testStakingInfo)
+	}
+
+	// should find correct stakingInfo with given block number
+	for i := uint64(1); i <= 4; i++ {
+		testStakingInfo := stakingCache.get(i)
+
+		if testStakingInfo.BlockNum != i {
+			t.Errorf("The block number of gotten staking info is diffrent. result : %v, expected : %v", testStakingInfo.BlockNum, i)
+		}
+	}
+
+	// nothing should be found as no matched block number is in cache
+	for i := uint64(5); i < 10; i++ {
+		testStakingInfo := stakingCache.get(i)
+
+		if testStakingInfo != nil {
+			t.Errorf("The result should be nil. result : %v", testStakingInfo)
+		}
 	}
 }
