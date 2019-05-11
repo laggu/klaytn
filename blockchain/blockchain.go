a5e23d3988a74b98acf8feff33a58678fb3ce264
Rick <rick.no@groundx.xyz>
2019-05-11 15:41:25 +0900

    change triesInMemory size (#3049)

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -67,7 +67,7 @@ const (
 )
 
 const (
-	triesInMemory = 128
+	triesInMemory = 4
 	// BlockChainVersion ensures that an incompatible database forces a resync from scratch.
 	BlockChainVersion    = 3
 	DefaultBlockInterval = 128
