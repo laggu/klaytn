55f2ea78239b55f9efd4a1cd9ad35ce562266d35
Jesse Jonghui Lee <jesse.lee@groundx.xyz>
2019-05-21 09:55:46 +0900

    Release klaytn v0.8.2-30 (#3253)

--- params/version.go
+++ params/version.go
@@ -23,10 +23,10 @@ package params
 import "fmt"
 
 const (
-	ReleaseNum   = 29
+	ReleaseNum   = 30
 	VersionMajor = 0 // Major version component of the current release
 	VersionMinor = 8 // Minor version component of the current release
-	VersionPatch = 1 // Patch version component of the current release
+	VersionPatch = 2 // Patch version component of the current release
 )
 
 // Version holds the textual version string.
