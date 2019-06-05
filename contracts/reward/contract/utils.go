d9992086c4c4cb1def793a76c34198f85b02b8d1
jk-jeongkyun <45347815+jeongkyun-oh@users.noreply.github.com>
2019-06-05 13:50:40 +0900

    Added cypress genesis picture (#3498)
    * Added cypress genesis picture
    
    * Renamed variables

--- contracts/reward/contract/utils.go
+++ contracts/reward/contract/utils.go
@@ -17,7 +17,8 @@
 package contract
 
 const (
-	AddressBookContractAddress = "0x0000000000000000000000000000000000000400"
+	CypressCreditContractAddress = "0x0000000000000000000000000000000000000000"
+	AddressBookContractAddress   = "0x0000000000000000000000000000000000000400"
 
 	// TODO-Klaytn-Issue1587 Decide whether to cleanup below three addresses after discussing issue1587
 	RewardContractAddress = "0x0000000000000000000000000000000000000441"
