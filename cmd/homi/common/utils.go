4d7105cac814e513fe6ae701076b0bb40d2bf4f7
Rick <rick.no@groundx.xyz>
2019-06-18 14:03:31 +0900

    fix license commemt (#3650)

--- cmd/homi/common/utils.go
+++ cmd/homi/common/utils.go
@@ -1,4 +1,4 @@
-// Copyright 2017 AMIS Technologgeries
+// Copyright 2017 AMIS Technologies
 // This file is part of the go-ethereum library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
