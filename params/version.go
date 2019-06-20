c7b7f454b8411dea9ea016cbaeaf677db68b4c46
Jesse Jonghui Lee <jesse.lee@groundx.xyz>
2019-06-20 16:44:14 +0900

    Release v0.10.1-40 (#3687)

--- params/version.go
+++ params/version.go
@@ -23,10 +23,10 @@ package params
 import "fmt"
 
 const (
-	ReleaseNum   = 39
+	ReleaseNum   = 40
 	VersionMajor = 0  // Major version component of the current release
 	VersionMinor = 10 // Minor version component of the current release
-	VersionPatch = 0  // Patch version component of the current release
+	VersionPatch = 1  // Patch version component of the current release
 )
 
 // Version holds the textual version string.
