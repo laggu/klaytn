f32d7c7a1212edf96db2e2d2222e65fb2ba6b314
jk-jeongkyun <45347815+jeongkyun-oh@users.noreply.github.com>
2019-06-25 11:28:27 +0900

    Updated node/cn/* package description (#3742)

--- node/cn/gasprice/doc.go
+++ node/cn/gasprice/doc.go
@@ -14,5 +14,5 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-// TODO-Klaytn-Docs: Write a package document
+// Package gasprice contains Oracle type which recommends gas prices based on recent blocks.
 package gasprice
